a6e5c2649ec97a6db9cc1034efd9bbcad93669fb
roosaap-sudo <rosevcreative@gmail.com>
2022-11-26 14:57:45 +0100

    respond

--- app/main.py
+++ app/main.py
@@ -10,7 +10,8 @@ def main():
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     server_socket.accept() # wait for client
-
+    data = server_socket.recv()
+    server_socket.send(b"hoi")
 
 if __name__ == "__main__":
     main()
